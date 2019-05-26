3e2fb27fa8b539f7994d7b483c1ab98dfb852e7e
peaceville <uferepeace@gmail.com>
2019-05-26 21:58:10 +0100

    docker container setup with python postgres and flask images. App ran locally and deployed to EC2 instance

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,25 +1,56 @@
 # services/users/project/__init__.py
 
-
+import os, sys
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
-app = Flask(__name__)
+# app = Flask(__name__)
 
-api = Api(app)
+# api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+# app.config.from_object('project.config.DevelopmentConfig')  # new
+# app_settings = os.getenv('APP_SETTINGS')  # new
+# app.config.from_object(app_settings)      # new
+
+# instantiate the db
+db = SQLAlchemy()  # new
+
+# print(app.config, file=sys.stderr)
+
+# new
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
+
+# class UsersPing(Resource):
+#     def get(self):
+#         return {
+#         'status': 'success',
+#         'message': 'pong!'
+#     }
 
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-        'status': 'success',
-        'message': 'pong!'
-    }
-
-
-api.add_resource(UsersPing, '/users/ping')
+# api.add_resource(UsersPing, '/users/ping')
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,18 +1,23 @@
+import os  # new
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
