848cac9db4377516d1412410570d30d459150aba
peaceville <uferepeace@gmail.com>
2019-05-26 22:41:29 +0100

    added code coverage and linting utility to app

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,4 +1,5 @@
 # services/users/manage.py
+import coverage
 import sys
 import unittest
 from flask.cli import FlaskGroup
@@ -7,6 +8,16 @@ from flask.cli import FlaskGroup
 
 from project import create_app, db   # new
 from project.api.models import User  # new
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 
 app = create_app()  # new
@@ -34,6 +45,21 @@ def seed_db():
     db.session.add(User(username='michael', email="hermanmu@gmail.com"))
     db.session.add(User(username='michaelherman', email="michael@mherman.org"))
     db.session.commit()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
